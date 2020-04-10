a3700912f83d1ae198f5dc1840d4bea8f9d38338
conantroutman <johsod96@gmail.com>
2020-04-10 22:49:41 +0200

    Implemented WIP CSV download functionality

--- greasemonkey.js
+++ greasemonkey.js
@@ -1,19 +1,22 @@
 // ==UserScript==
 // @name     Unnamed Script 788987
 // @version  1
-// @grant    none
+// @grant    GM.getValue
+// @grant    GM.setValue
+// @include  http://127.0.0.1:5500*
 // ==/UserScript==
 
 var count;
-var numbers = [];
+var numbers = [100, 200, 300];
 var intervalTime = 1000;
 var interval;
 
 window.addEventListener(
 	"load",
 	function () {
-		count = 0;
-		interval = setInterval(measureFPS, intervalTime);
+		//count = 0;
+		//interval = setInterval(measureFPS, intervalTime);
+		downloadCSV();
 	},
 	false
 );
@@ -27,14 +30,17 @@ function measureFPS() {
 		numbers.push(parseInt(document.getElementsByTagName("div")[1].textContent));
 		console.log(numbers);
 	}
+	console.log("Test");
 }
 
-function getData() {
-	// Make anchor and click it!
-	var anchor = document.createElement("a");
-	anchor.setAttribute("href", encodeURI(numbers));
-	anchor.setAttribute("download", "my_data.csv");
-	anchor.innerHTML = "Click Here to download";
-	document.body.appendChild(anchor);
-	anchor.click();
+function downloadCSV() {
+	var data = numbers.toString();
+	var a = document.createElement("a");
+	a.href = "data:application/csv;charset=utf-8," + encodeURIComponent(data);
+	//supported by chrome 14+ and firefox 20+
+	a.download = "data.csv";
+	//needed for firefox
+	document.getElementsByTagName("body")[0].appendChild(a);
+	//supported by chrome 20+ and firefox 5+
+	a.click();
 }
