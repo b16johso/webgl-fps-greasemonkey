081e7cdaffa2d8b81904f819c174c890812ade89
conantroutman <johsod96@gmail.com>
2020-04-14 12:14:58 +0200

    Script is triggered by mouse click

--- greasemonkey.js
+++ greasemonkey.js
@@ -8,22 +8,30 @@
 
 var timesRun;
 var timesToRun = 10;
-var intervalTime = 1000;
 var fps = [];
+var intervalTime = 1000;
+var interval;
+var isRunning = false;
 
 window.addEventListener(
-	"load",
+	"click",
 	function () {
-		timesRun = 0;
-		var interval = setInterval(function () {
-			timesRun++;
-			if (timesRun === timesToRun) {
-				downloadCSV();
-				clearInterval(interval);
-			}
-			fps.push(parseInt(document.getElementsByTagName("div")[1].textContent));
-			console.log(fps);
-		}, intervalTime);
+		if (!isRunning) {
+			isRunning = true;
+			timesRun = 0;
+			interval = setInterval(function () {
+				timesRun++;
+				fps.push(parseInt(document.getElementsByTagName("div")[1].textContent));
+				console.log(fps);
+
+				if (timesRun === timesToRun) {
+					//downloadCSV();
+					clearInterval(interval);
+					isRunning = false;
+					console.log("Ran " + timesRun + " times");
+				}
+			}, intervalTime);
+		}
 	},
 	false
 );
@@ -32,7 +40,10 @@ function downloadCSV() {
 	var data = fps.toString();
 	var a = document.createElement("a");
 	a.href = "data:application/csv;charset=utf-8," + encodeURIComponent(data);
+	//supported by chrome 14+ and firefox 20+
 	a.download = "data.csv";
+	//needed for firefox
 	document.getElementsByTagName("body")[0].appendChild(a);
+	//supported by chrome 20+ and firefox 5+
 	a.click();
 }
