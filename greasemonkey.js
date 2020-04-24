54b68f98e8237a14d94b28f402a50f77e33e4941
b16johso <b16johso@student.his.se>
2020-04-24 16:42:38 +0200

    FOrgot to push

--- greasemonkey.js
+++ greasemonkey.js
@@ -4,6 +4,7 @@
 // @grant    GM.getValue
 // @grant    GM.setValue
 // @include  http://127.0.0.1:5500/*
+// @include  http://127.0.0.1:5501/*
 // ==/UserScript==
 
 var timesRun;
@@ -14,50 +15,50 @@ var interval;
 var isRunning = false;
 
 window.addEventListener(
-	"pointerdown",
-	function () {
-		if (!isRunning) {
-			isRunning = true;
-			timesRun = 0;
-			interval = setInterval(function () {
-				timesRun++;
-				fps.push(parseInt(document.getElementsByTagName("div")[1].textContent));
-				console.log(fps);
+  "pointerdown",
+  function () {
+    if (!isRunning) {
+      isRunning = true;
+      timesRun = 0;
+      interval = setInterval(function () {
+        timesRun++;
+        fps.push(parseInt(document.getElementsByTagName("div")[1].textContent));
+        console.log(fps);
 
-				/*if (timesRun === timesToRun) {
+        /*if (timesRun === timesToRun) {
 					downloadCSV();
 					resetScript();
 					console.log("Ran " + timesRun + " times");
 				}*/
-			}, intervalTime);
-		}
-	},
-	false
+      }, intervalTime);
+    }
+  },
+  false
 );
 
 window.addEventListener(
-	"keydown",
-	function (e) {
-		if (e.code === "KeyJ") {
-			console.log("Pressed J");
-			downloadCSV();
-			resetScript();
-		}
-	},
-	false
+  "keydown",
+  function (e) {
+    if (e.code === "KeyJ") {
+      console.log("Pressed J");
+      downloadCSV();
+      resetScript();
+    }
+  },
+  false
 );
 
 function resetScript() {
-	clearInterval(interval);
-	fps = [];
-	isRunning = false;
+  clearInterval(interval);
+  fps = [];
+  isRunning = false;
 }
 
 function downloadCSV() {
-	var data = fps.toString();
-	var link = document.createElement("a");
-	link.href = "data:application/csv;charset=utf-8," + encodeURIComponent(data);
-	link.download = "data.csv";
-	document.getElementsByTagName("body")[0].appendChild(link);
-	link.click();
+  var data = fps.toString();
+  var link = document.createElement("a");
+  link.href = "data:application/csv;charset=utf-8," + encodeURIComponent(data);
+  link.download = "data.csv";
+  document.getElementsByTagName("body")[0].appendChild(link);
+  link.click();
 }
